399caaa4959db6b9884cb6c2ba3817b15677263b
Jonathan Cross <jonathan.a.cross@gmail.com>
2017-03-25 00:37:46 -0700

    adding version number to load screen

--- src/main/java/pow/frontend/window/WelcomeWindow.java
+++ src/main/java/pow/frontend/window/WelcomeWindow.java
@@ -8,11 +8,19 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class WelcomeWindow extends AbstractWindow {
 
+    private String version;
+
     public WelcomeWindow(WindowDim dim, boolean visible, GameBackend backend, Frontend frontend) {
         super(dim, visible, backend, frontend);
+        version = getVersion();
     }
 
     @Override
@@ -34,6 +42,18 @@ public class WelcomeWindow extends AbstractWindow {
         f = new Font("Courier", Font.PLAIN, squareSize);
         graphics.setFont(f);
         graphics.setColor(Color.WHITE);
+
+        graphics.drawString(version, 30, 100);
         graphics.drawString("Press any key to begin.", 30, 150);
+    }
+
+    private String getVersion() {
+        Properties prop = new Properties();
+        try (InputStream input = this.getClass().getResourceAsStream("/application.properties")) {
+            prop.load(input);
+            return prop.getProperty("application.version");
+        } catch (IOException e) {
+            return e.toString();
+        }
     }
 }
