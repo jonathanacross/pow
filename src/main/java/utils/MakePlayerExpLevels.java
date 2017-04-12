645aeaedd813a4859e8729163245ed1ea3f6e938
Jonathan Cross <jonathan.a.cross@gmail.com>
2017-04-12 00:00:54 -0700

    making things easier: monsters do less damage, and player needs less exp

--- src/main/java/utils/MakePlayerExpLevels.java
+++ src/main/java/utils/MakePlayerExpLevels.java
@@ -8,14 +8,11 @@ import java.util.*;
 
 public class MakePlayerExpLevels {
 
-    // Thoughts on how to compute experience levels:
-    // 1. take avg monster experience for each level.
+    // To compute experience levels:
+    // 1. take avg monster experience (non-bosses) for each level, and curve fit.
     // 2. compute approx desired number of monsters to kill each level.
-    //    current thought is something like ln(level + 0.1)*75.
-    // 3. multiply, then curve fit.
-
-    // Current estimate is: let y = exp to next level, and x = level.  Then
-    // ln(y) = 3.78*x^0.367
+    //    current thought is something like ln(level + 0.2)*50.
+    // 3. multiply.
 
     // If it's too easy to go up levels using current monsters, then
     // consider Angband's approach: compute monster exp by multiplying
@@ -31,7 +28,7 @@ public class MakePlayerExpLevels {
         return expTotal / count;
     }
 
-    public static void showAverageMonsterExpByLevel() {
+    public static CurveFit.Params computeAverageMonsterExpByLevel() {
         Random rng = new Random(123);
         Set<String> allMonsters = MonsterGenerator.getMonsterIds();
 
@@ -39,6 +36,8 @@ public class MakePlayerExpLevels {
         for (String id: allMonsters) {
             Monster m = MonsterGenerator.genMonster(id, rng, new Point(-1,-1));
             int level = m.level;
+            // hacky way to skip bosses
+            if (m.requiredItemDrops != null) continue;
 
             if (!monstersByLevel.containsKey(level)) {
                 monstersByLevel.put(level, new ArrayList<>());
@@ -50,21 +49,33 @@ public class MakePlayerExpLevels {
         levels.addAll(monstersByLevel.keySet());
         Collections.sort(levels);
         for (int level : levels) {
+//            for (Monster m: monstersByLevel.get(level)) {
+//                System.out.println(level + "\t" + m.name + "\t" + m.experience);
+//            }
             System.out.println(level + "\t" + getAvgExperience(monstersByLevel.get(level)));
         }
+
+        double x[] = new double[levels.size()];
+        double y[] = new double[levels.size()];
+        for (int i = 0; i < levels.size(); i++) {
+            x[i] = levels.get(i);
+            y[i] = getAvgExperience(monstersByLevel.get(i));
+        }
+        CurveFit.Params monsterExpParams = CurveFit.expFit(x, y);
+        System.out.println("curve fit: avgExp = " + monsterExpParams.a + " * exp(" + monsterExpParams.b + " * x)");
+        return monsterExpParams;
     }
 
     private static void makeCharacterExperienceTable() {
-        // probably want this higher than the factor in baseHP for monster health, in makeMonsterStats
-        // but not so much that it's a grind..
-//        int firstLevelNeeded = 10;
-//        double increaseRate = 1.45;
-        int numLevels = 21;
+        CurveFit.Params monsterExpParams = computeAverageMonsterExpByLevel();
 
+        int numLevels = 21;
         int[] amountToReachNextLevel = new int[numLevels];
         int[] total = new int[numLevels];
         for (int i = 0; i < numLevels; i++) {
-            amountToReachNextLevel[i] = (int) Math.round(Math.exp(3.78*Math.pow(i,0.367)));
+            double desiredMonstersToKill = 50.0 * Math.log(i + 0.2);
+            double expPerMonster = monsterExpParams.a * Math.exp(monsterExpParams.b * i);
+            amountToReachNextLevel[i] = (int) Math.round(desiredMonstersToKill * expPerMonster);
             if (i > 0) {
                 total[i] = total[i-1] + amountToReachNextLevel[i];
             }
@@ -76,7 +87,6 @@ public class MakePlayerExpLevels {
     }
 
     public static void main(String[] args) {
-        //showAverageMonsterExpByLevel();
         makeCharacterExperienceTable();
     }
 }
