3d2fb94594d0b2436a0898622750d9f6b12d6d04
Jonathan Cross <jonathan.a.cross@gmail.com>
2019-01-09 22:28:50 -0800

    hiding mouse after a keypress

--- src/main/java/pow/ui/MainWindow.java
+++ src/main/java/pow/ui/MainWindow.java
@@ -1,17 +1,28 @@
 package pow.ui;
 
 import javax.swing.*;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 
 
-public class MainWindow extends JFrame implements KeyListener {
+public class MainWindow extends JFrame implements KeyListener, MouseMotionListener {
     private final MainDraw draw;
+
+    private static Cursor blankCursor;
+    static {
+        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+                cursorImg, new Point(0, 0), "blank cursor");
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
         draw.processKey(e);
+        this.setCursor(blankCursor);
     }
 
     @Override
@@ -31,8 +42,19 @@ public class MainWindow extends JFrame implements KeyListener {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         draw = new MainDraw();
         getContentPane().add(draw);
+        addMouseMotionListener(this);
         pack();
 
         setVisible(true);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        this.setCursor(Cursor.getDefaultCursor());
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        this.setCursor(Cursor.getDefaultCursor());
     }
 }
