1c72833ab9bd0a59589b09587f5f7693c303f082
Jonathan Cross <jonathan.a.cross@gmail.com>
2019-04-17 22:47:27 -0700

    bugfix: resist elements had no effect!

--- src/main/java/pow/backend/utils/AttackUtils.java
+++ src/main/java/pow/backend/utils/AttackUtils.java
@@ -26,12 +26,12 @@ public class AttackUtils {
     public static int adjustDamage(int baseDamage, SpellParams.Element element, Actor defender) {
         int bonus;
         switch (element) {
-            case FIRE: bonus = defender.baseStats.resFire; break;
-            case ICE: bonus = defender.baseStats.resCold; break;
-            case ACID: bonus = defender.baseStats.resAcid; break;
-            case LIGHTNING: bonus = defender.baseStats.resElec; break;
-            case POISON: bonus = defender.baseStats.resPois; break;
-            case DAMAGE: bonus = defender.baseStats.resDam; break;
+            case FIRE: bonus = defender.getResFire(); break;
+            case ICE: bonus = defender.getResCold(); break;
+            case ACID: bonus = defender.getResAcid(); break;
+            case LIGHTNING: bonus = defender.getResElec(); break;
+            case POISON: bonus = defender.getResPois(); break;
+            case DAMAGE: bonus = defender.getResDam(); break;
             default: bonus = 0;
         }
 
--- src/main/java/pow/frontend/window/PlayerInfoWindow.java
+++ src/main/java/pow/frontend/window/PlayerInfoWindow.java
@@ -110,12 +110,12 @@ public class PlayerInfoWindow extends AbstractWindow {
         table.addRow(getRow("Defense:   ", "" + player.getDefense(), font));
         table.addRow(getRow("Speed:     ", "" + player.getSpeed(), font));
         table.addRow(getRow("", "", font));
-        table.addRow(getRow("rFire:     ", "" + getResistPercent(player.baseStats.resFire), font));
-        table.addRow(getRow("rCold:     ", "" + getResistPercent(player.baseStats.resCold), font));
-        table.addRow(getRow("rAcid:     ", "" + getResistPercent(player.baseStats.resAcid), font));
-        table.addRow(getRow("rElec:     ", "" + getResistPercent(player.baseStats.resElec), font));
-        table.addRow(getRow("rPois:     ", "" + getResistPercent(player.baseStats.resPois), font));
-        table.addRow(getRow("rDam:      ", "" + getResistPercent(player.baseStats.resDam), font));
+        table.addRow(getRow("rFire:     ", "" + getResistPercent(player.getResFire()), font));
+        table.addRow(getRow("rCold:     ", "" + getResistPercent(player.getResCold()), font));
+        table.addRow(getRow("rAcid:     ", "" + getResistPercent(player.getResAcid()), font));
+        table.addRow(getRow("rElec:     ", "" + getResistPercent(player.getResElec()), font));
+        table.addRow(getRow("rPois:     ", "" + getResistPercent(player.getResPois()), font));
+        table.addRow(getRow("rDam:      ", "" + getResistPercent(player.getResDam()), font));
         table.addRow(getRow("", "", font));
         if (!player.spells.isEmpty()) {
             String spellsString = "Spells: " + TextUtils.formatList(getSpellNames(player.spells)) + ".";
